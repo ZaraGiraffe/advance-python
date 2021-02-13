126f24955f3b4937b887ceb1526b4dfac14f3fb2
zahar <znaumets@gmail.com>
2021-02-13 13:20:13 +0200

    add range.py

--- range.py
+++ range.py
@@ -1,20 +1,40 @@
-class My_range:
-    def __init__(self, start: int, step: int, stop: int):
-        self.start = start - step
-        self.step = step
-        self.stop = stop
+class MyRange:
+    def __init__(self, *args):
+        if len(args) == 1:
+            self.stop = args[0]
+            self.step = 1
+            self.start = 0 - self.step
+        elif len(args) == 2:
+            self.step = 1
+            self.start = args[0] - self.step
+            self.stop = args[1]
+        elif len(args) == 3:
+            self.step = args[2]
+            self.start = args[0] - self.step
+            self.stop = args[1]
+        else:
+            raise IndexError('args takes only three arguments')
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        self.start += self.step
-        if self.start < self.stop:
-            return self.start
+        if self.step > 0:
+            self.start += self.step
+            if self.start < self.stop:
+                return self.start
+            else:
+                raise StopIteration
+        elif self.step < 0:
+            self.start += self.step
+            if self.start > self.stop:
+                return self.start
+            else:
+                raise StopIteration
         else:
-            raise StopIteration
+            raise ValueError('step should not be zero')
 
 
-for i in My_range(3, 12, 66):
+for i in MyRange(23, 57, 4):
     print(i)
 
